e5f962c8aeb6c46e701464f607db63fe1479d519
quinise <devinercolano@Devins-MacBook-Pro.local>
2023-04-12 10:44:09 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HistoryComponent } from './history/history.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'history', component: HistoryComponent},
+  { path: 'appointments', component: AppointmentsComponent},
+  { path: 'contact', component: ContactComponent}
+
+];
 
 @NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    HistoryComponent,
+    AppointmentsComponent,
+    ContactComponent,
+  ],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  bootstrap: [
+    HomeComponent,
+    AboutComponent,
+    HistoryComponent,
+    AppointmentsComponent,
+    ContactComponent
+    ]
 })
 export class AppRoutingModule { }

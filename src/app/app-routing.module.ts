99f4bcd92f01742ee738da4232a005fa60b19e4e
quinise <quinise.ercolano@gmail.com>
2023-09-05 12:26:15 -0700

    added comments to the files I edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+// This file defines the app's routes
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
